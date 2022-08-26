bb813f1406932eb8ba06dcb2ae575417dee9f932
Tfstar <pacesetter05@gmail.com>
2022-08-25 21:08:52 -0700

    Import module

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """prints the number of and the list of its arguments"""
+    import sys
+
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
